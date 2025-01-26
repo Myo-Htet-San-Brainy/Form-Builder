41bf8219b48305c2e37be3c75bf3f39adb3245a4
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2025-01-26 20:16:50 +0700

    fixed minor bug

--- app/lib/formStore.ts
+++ app/lib/formStore.ts
@@ -1,4 +1,4 @@
-import { create } from "Zustand";
+import { create } from "zustand";
 import { v4 as uuidv4 } from "uuid";
 import { Form } from "../entities/form";
 import { convertFieldType, FieldTypes } from "../utils/index";
