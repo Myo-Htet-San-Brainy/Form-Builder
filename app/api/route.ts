04e8decb643e2f4b2bb8c7789a1751fbd3d58e96
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2025-03-11 23:22:42 +0700

    fixing netlify next build errs

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,62 +1,28 @@
 import { NextResponse, type NextRequest } from "next/server";
 import client from "../lib/mongodb";
 import { refineData } from "../utils";
-// import { MongoClient } from "mongodb";
-
-// export async function POST(request: NextRequest) {
-//   console.log("POST - Inserting quiz");
-//   try {
-//     const body = await request.json();
-//     //refinement
-//     const quiz = refineData(body);
-
-//     const QuizDb = client.db("Quiz");
-//     const quizCollection = QuizDb.collection("quiz");
-
-//     const result = await quizCollection.insertOne(quiz);
-
-//     if (!result.acknowledged) {
-//       return NextResponse.json(
-//         { error: "Insert operation was not successful" },
-//         { status: 500 } // Internal Server Error
-//       );
-//     }
-
-//     return NextResponse.json(
-//       { data: result },
-//       { status: 201 } // Created
-//     );
-//   } catch (error) {
-//     console.log("err inserting quiz", error);
-//     return NextResponse.json(
-//       { error: "An unexpected error occurred" },
-//       { status: 500 }
-//     );
-//   }
-// }
+export const dynamic = "force-dynamic";
 
 export async function POST(request: NextRequest) {
-  console.log("POST - Inserting quiz");
   try {
     const body = await request.json();
     //refinement
     const quiz = refineData(body);
 
-    console.log(client);
-    // const QuizDb = client.db("Quiz");
-    // const quizCollection = QuizDb.collection("quiz");
+    const QuizDb = client.db("Quiz");
+    const quizCollection = QuizDb.collection("quiz");
 
-    // const result = await quizCollection.insertOne(quiz);
+    const result = await quizCollection.insertOne(quiz);
 
-    // if (!result.acknowledged) {
-    //   return NextResponse.json(
-    //     { error: "Insert operation was not successful" },
-    //     { status: 500 } // Internal Server Error
-    //   );
-    // }
+    if (!result.acknowledged) {
+      return NextResponse.json(
+        { error: "Insert operation was not successful" },
+        { status: 500 } // Internal Server Error
+      );
+    }
 
     return NextResponse.json(
-      { data: "result" },
+      { data: result },
       { status: 201 } // Created
     );
   } catch (error) {
